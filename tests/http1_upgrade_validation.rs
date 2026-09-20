f992b38c70fc817dad49a7021cd0bacd5a0fb6c6
sunlei <guizaicn@gmail.com>
2026-09-21 00:21:38 +0800

    test: cover valid HTTP upgrade header forms
    - accept optional whitespace around required field values
    - accept case-insensitive upgrade tokens in comma-separated lists

--- tests/http1_upgrade_validation.rs
+++ tests/http1_upgrade_validation.rs
@@ -49,6 +49,23 @@ fn request_requires_a_base64_encoded_16_byte_key() {
 }
 
 #[test]
+fn request_accepts_ows_around_required_header_values() {
+    let request = b"GET /ws HTTP/1.1\r\n\
+        Host:\t example.com \t\r\n\
+        Upgrade: WebSocket\r\n\
+        Connection: keep-alive, UpGrAdE\r\n\
+        Sec-WebSocket-Key:\t dGhlIHNhbXBsZSBub25jZQ== \t\r\n\
+        Sec-WebSocket-Version:\t 13 \t\r\n\
+        \r\n";
+
+    let (request, _) = parse_request(request).unwrap().unwrap();
+
+    assert_eq!(request.host, Some("example.com"));
+    assert_eq!(request.key, KEY);
+    assert_eq!(request.version, "13");
+}
+
+#[test]
 fn response_requires_http_11() {
     let response = b"HTTP/1.0 101 Switching Protocols\r\n\
         Upgrade: websocket\r\n\
@@ -75,4 +92,14 @@ fn response_requires_upgrade_and_connection_tokens() {
     ] {
         assert!(parse_response(response).is_err());
     }
+}
+
+#[test]
+fn response_accepts_case_insensitive_tokens_in_lists() {
+    let response = b"HTTP/1.1 101 Switching Protocols\r\n\
+        Upgrade:\t h2c, WebSocket \t\r\n\
+        Connection: keep-alive, UpGrAdE\r\n\
+        \r\n";
+
+    assert!(parse_response(response).unwrap().is_some());
 }
